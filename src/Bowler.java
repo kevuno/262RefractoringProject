dd1b07c9977b9c59fa16f3004bbec090d5e653a2
exw4141 <exw4141@rit.edu>
2017-04-26 14:43:24 -0400

    Readded the equals() method in Bowler

--- src/Bowler.java
+++ src/Bowler.java
@@ -53,4 +53,17 @@ public class Bowler {
 		return email;	
 	}
 
+	public boolean equals ( Bowler b) {
+		boolean retval = true;
+		if ( !(nickName.equals(b.getNickName())) ) {
+				retval = false;
+		}
+		if ( !(fullName.equals(b.getFullName())) ) {
+				retval = false;
+		}
+		if ( !(email.equals(b.getEmail())) ) {
+				retval = false;
+		}
+		return retval;
+	}
 }
