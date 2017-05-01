964bb1aa3c105220d2eba65b325bc634ff95c260
Jacob <jcy5214@g.rit.edu>
2017-05-01 14:23:07 -0400

    Merge branch 'master' of C:\Users\Jacob\Documents\262RefractoringProject with conflicts.

--- src/Bowler.java
+++ src/Bowler.java
@@ -19,40 +19,51 @@
 
 /**
  *  Class that holds all bowler info
+ *
  */
 
 public class Bowler {
 
-	private String fullName;
-	private String nickName;
-	private String email;
+    private String fullName;
+    private String nickName;
+    private String email;
 
-	public Bowler(String nick, String full, String mail) {
-		nickName = nick;
-		fullName = full;
-		email = mail;
+    public Bowler( String nick, String full, String mail ) {
+	nickName = nick;
+	fullName = full;
+  	email = mail;
+    }
+
+
+    public String getNickName() {
+
+        return nickName;  
+
+    }
+
+	public String getFullName ( ) {
+			return fullName;
 	}
-
-	public String getNickName() {
+	
+	public String getNick ( ) {
 		return nickName;
 	}
 
-	public String getFullName() {
-		return fullName;
-	}
-
-	public String getEmail() {
+	public String getEmail ( ) {
 		return email;	
 	}
 
-	@Override
-	public boolean equals(Object other) {
-		if (other == null || !Bowler.class.isAssignableFrom(other.getClass())) {
-			return false;
+	public boolean equals ( Bowler b) {
+		boolean retval = true;
+		if ( !(nickName.equals(b.getNickName())) ) {
+			retval = false;
 		}
-		final Bowler otherBowler = (Bowler) other;
-		return nickName.equals(otherBowler.getNickName())
-				&& fullName.equals(otherBowler.getFullName())
-				&& email.equals(otherBowler.getEmail());
+		if ( !(fullName.equals(b.getFullName())) ) {
+			retval = false;
+		}
+		if ( !(email.equals(b.getEmail())) ) {
+			retval = false;
+		}
+		return retval;
 	}
 }
