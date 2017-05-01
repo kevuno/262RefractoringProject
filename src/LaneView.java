ad930cbe496e6689c17e8d1c016c646e6cac96fd
Nicholas <nickcfeldman@gmail.com>
2017-05-01 14:24:44 -0400

    Make some abomination in LaneView::receiveLaneEvent() almost readable

--- src/LaneView.java
+++ src/LaneView.java
@@ -163,40 +163,26 @@ public class LaneView implements LaneObserver, ActionListener {
 			int[][] lescores = le.getCumulScore();
 			for (int k = 0; k < numBowlers; k++) {
 				for (int i = 0; i <= le.getFrameNum() - 1; i++) {
-					if (lescores[k][i] != 0)
-						scoreLabel[k][i].setText(
-							(new Integer(lescores[k][i])).toString());
+					if (lescores[k][i] != 0) {
+						scoreLabel[k][i].setText((new Integer(lescores[k][i])).toString());
+					}
 				}
+
 				for (int i = 0; i < 21; i++) {
-					if (((int[]) (le.getScore())
-						.get(bowlers.get(k)))[i]
-						!= -1)
-						if (((int[]) (le.getScore())
-							.get(bowlers.get(k)))[i]
-							== 10
-							&& (i % 2 == 0 || i == 19))
+					if (((int[]) (le.getScore()).get(bowlers.get(k)))[i] != -1) {
+						if (((int[]) (le.getScore()).get(bowlers.get(k)))[i] == 10 && (i % 2 == 0 || i == 19)) {
 							ballLabel[k][i].setText("X");
-						else if (
-							i > 0
-								&& ((int[]) (le.getScore())
-									.get(bowlers.get(k)))[i]
-									+ ((int[]) (le.getScore())
-										.get(bowlers.get(k)))[i
-									- 1]
-									== 10
-								&& i % 2 == 1)
+						} else if (i > 0 && ((int[]) (le.getScore()).get(bowlers.get(k)))[i] +
+								((int[]) (le.getScore()).get(bowlers.get(k)))[i - 1] == 10 && i % 2 == 1) {
 							ballLabel[k][i].setText("/");
-						else if ( ((int[])(le.getScore()).get(bowlers.get(k)))[i] == -2 ){
-							
+						} else if (((int[]) (le.getScore()).get(bowlers.get(k)))[i] == -2) {
 							ballLabel[k][i].setText("F");
-						} else
-							ballLabel[k][i].setText(
-								(new Integer(((int[]) (le.getScore())
-									.get(bowlers.get(k)))[i]))
-									.toString());
+						} else {
+							ballLabel[k][i].setText((new Integer(((int[]) (le.getScore()).get(bowlers.get(k)))[i])).toString());
+						}
+					}
 				}
 			}
-
 		}
 	}
 
